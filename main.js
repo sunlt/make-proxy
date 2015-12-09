2c944e06938aec15b71b0671f6dbcbf9b4b634e7
suntl <sunliutao@foxmail.com>
2015-12-09 17:39:41 +0800

    first

---
+++ main.js
@@ -0,0 +1,50 @@
+
+
+var fs = require('fs');
+var connect = require('connect');
+var url = require('url');
+var proxy = require('proxy-middleware');
+var program = require('commander');
+var hostile = require('hostile');
+
+program
+  .version('0.0.1')
+  .option('-t, --target', 'target server [http://127.0.0.1:8080] ')
+  .option('-p, --port', 'listen port [80]')
+  .option('-h, --host', 'bind domain to hosts file [wwww.google.com]')
+  .option('-d, --dir', 'origin virtual directory [/dir1/....]')
+  .parse(process.argv);
+
+
+var target = program.target;
+var port = program.port || 80;
+var host = program.host;
+var dir = program.dir || '/';
+
+if(!host && !target){
+  return;
+}
+
+hostile.set('127.0.0.1', host);
+console.log('set hosts successfully!');
+
+
+process.on('exit', function(code) {
+  hostile.remove('127.0.0.1',host);
+  console.log('remove hosts successfully!');
+});
+
+process.on('SIGINT', function() {
+  process.exit();
+});
+process.on('SIGTERM', function() {
+  process.exit();
+});
+
+var app = connect();
+app.use(dir, proxy(url.parse(target)));
+
+app.listen(port);
+
+
+console.log('listen port:' + port);
